7ba2c56b415ca1b0e7781bff2ceb85a4f408b149
rnbsrva <orynbasarovaakerke88@gmail.com>
2023-11-16 15:04:23 +0600

    banner modes

--- auth-server/src/main/java/com/akerke/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/akerke/authserver/AuthServerApplication.java
@@ -3,14 +3,9 @@ package com.akerke.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class AuthServerApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
     }
-
-
 }
--- auth-server/src/main/java/com/akerke/authserver/controller/AuthController.java
+++ auth-server/src/main/java/com/akerke/authserver/controller/AuthController.java
@@ -2,33 +2,35 @@ package com.akerke.authserver.controller;
 
 import com.akerke.authserver.domain.dto.AuthDTO;
 import com.akerke.authserver.domain.dto.RegistrationDTO;
-import com.akerke.authserver.domain.model.User;
 import com.akerke.authserver.domain.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
-//@RestController
-//@RequestMapping("api/v1/auth")
-//@RequiredArgsConstructor
-//public class AuthController {
-//
-//    private final AuthService authService;
-//
-//    @PostMapping("register")
-//    Mono<User> register(
-//            @RequestBody RegistrationDTO registrationDTO
-//    ) {
-//        return authService.register(registrationDTO);
-//    }
-//
-//    @PostMapping
-//    Mono<?> authenticate(
-//            @RequestBody AuthDTO authDTO
-//            ){
-//        return authService.authenticate(authDTO);
-//    }
-//}
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("register")
+    ResponseEntity<?> register(
+            @RequestBody RegistrationDTO dto
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(authService.register(dto));
+    }
+
+    @PostMapping
+    ResponseEntity<?> authenticate(
+            @RequestBody AuthDTO dto
+    ) {
+        return ResponseEntity
+                .ok(authService.authenticate(dto));
+    }
+}
