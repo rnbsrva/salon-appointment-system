b892c774a02f8065ea707361ce1e9b6ca53332aa
rnbsrva <orynbasarovaakerke88@gmail.com>
2023-11-22 00:38:16 +0600

    auth server logging

--- auth-server/src/main/java/com/akerke/authserver/config/WebConfig.java
+++ auth-server/src/main/java/com/akerke/authserver/config/WebConfig.java
@@ -1,12 +1,18 @@
 package com.akerke.authserver.config;
 
+import com.akerke.authserver.common.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
 
     @Value("${spring.cors.mapping}")
     private String mapping;
@@ -20,5 +26,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor).addPathPatterns("/**");
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
