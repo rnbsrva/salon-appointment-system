dcadb7436ca0dedbd607fecf19a159f6edefee73
rnbsrva <orynbasarovaakerke88@gmail.com>
2023-12-04 20:50:27 +0600

    check commit

--- mail-sender/src/main/java/com/akerke/mailsender/MailSenderApplication.java
+++ mail-sender/src/main/java/com/akerke/mailsender/MailSenderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MailSenderApplication {
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(MailSenderApplication.class, args);
     }
 }
