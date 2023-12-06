61a733873ba87f250c67a9d4f1395cb4152b37da
rnbsrva <123613166+rnbsrva@users.noreply.github.com>
2023-12-06 16:06:24 +0600

    f

--- auth-server/src/main/java/com/akerke/authserver/domain/dto/AuthDTO.java
+++ auth-server/src/main/java/com/akerke/authserver/domain/dto/AuthDTO.java
@@ -2,9 +2,7 @@ package com.akerke.authserver.domain.dto;
 
 
 public record AuthDTO(
-//        @Email
         String email,
-//        @Password
         String password
 ) {
 }
--- storage-service/src/main/java/com/akerke/storageservice/common/feign/SalonServiceFeignClient.java
+++ storage-service/src/main/java/com/akerke/storageservice/common/feign/SalonServiceFeignClient.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "salon-service")
 public interface SalonServiceFeignClient {
 
-    @RequestMapping(method = RequestMethod.PUT, value = "salon/add-image/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/salon-service/salon/add-image/{id}")
     ResponseEntity<?> addImageToSalon(
             @RequestParam String imageId,
             @PathVariable Long id,
             @RequestParam Boolean isMainImage
     );
 
-    @RequestMapping(method = RequestMethod.PUT, value = "master/add-image/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/salon-service/master/add-image/{id}")
     ResponseEntity<?> addImageToMaster(
             @RequestParam String imageId,
             @PathVariable Long id,
             @RequestParam Boolean isMainImage
     );
 
-    @DeleteMapping("salon/delete-image/{imageId}")
+    @DeleteMapping("/salon-service/salon/delete-image/{imageId}")
     ResponseEntity<?> deleteImageOfSalon(
             @PathVariable String imageId
     );
 
-    @DeleteMapping("master/delete-image/{imageId}")
+    @DeleteMapping("/salon-service/master/delete-image/{imageId}")
     ResponseEntity<?> deleteImageOfMaster(
             @PathVariable String imageId
     );
