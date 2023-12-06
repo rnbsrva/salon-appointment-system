d3b8c66fcffb9d9f1d58df384bc69f08569bcf1b
rnbsrva <123613166+rnbsrva@users.noreply.github.com>
2023-12-06 13:49:59 +0600

    manual config notification servicew

--- notification-service/src/main/java/com/akerke/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/akerke/notificationservice/NotificationServiceApplication.java
@@ -1,17 +1,25 @@
 package com.akerke.notificationservice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableFeignClients
-public class NotificationServiceApplication {
+public class NotificationServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NotificationServiceApplication.class);
     }
 
 }
